a33764611666dde524e18e055f93fd33dc7079b3
Aurélien Vieville <oc.aurelien.vieville@gmail.com>
2023-11-24 14:49:41 +0100

    Created an express.js project, add connection with mongodb, add User model, registration and connection functions , jwt and dotenv.

---
+++ app.js
@@ -0,0 +1,36 @@
+const express = require("express");
+
+const mongoose = require("mongoose");
+
+require('dotenv').config({path: "./.env"});
+
+const userRoutes = require("./routes/user");
+
+mongoose
+  .connect(
+    process.env.MONGODB_URL,
+    { useNewUrlParser: true, useUnifiedTopology: true }
+  )
+  .then(() => console.log("Connexion à MongoDB réussie !"))
+  .catch(() => console.log("Connexion à MongoDB échouée !"));
+
+const app = express();
+
+app.use(express.json());
+
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content, Accept, Content-Type, Authorization"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PUT, DELETE, PATCH, OPTIONS"
+  );
+  next();
+});
+
+app.use("/api/auth", userRoutes);
+
+module.exports = app;
