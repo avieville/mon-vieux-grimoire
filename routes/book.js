a9b79285c71c2186bae2abd3599bed18c32f8f38
Aurélien Vieville <oc.aurelien.vieville@gmail.com>
2023-11-27 12:02:32 +0100

    added reading a book, modified a book, deleted a book

--- routes/book.js
+++ routes/book.js
@@ -6,7 +6,10 @@ const multer = require("../middleware/multer-config");
 
 const bookCtrl = require("../controllers/book");
 
-router.get("/", bookCtrl.getAllBook);
 router.post("/", auth, multer, bookCtrl.createBook);
+router.put("/:id", auth, multer, bookCtrl.modifyBook);
+router.get("/:id", bookCtrl.getOneBook);
+router.get("/", bookCtrl.getAllBook);
+router.delete("/:id", auth, bookCtrl.deleteBook);
 
 module.exports = router;
